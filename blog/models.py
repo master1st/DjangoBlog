91210cc50d1f10586a1878db4e0f7c56737ffa2b
seokhyun <gah1234@naver.com>
2022-03-11 19:35:43 +0900

    blog/post 모델 생성

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    create_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+# Create your models here.
+
+
+def __str__(self):
+    return f'[{self.pk}]{self.title}'
