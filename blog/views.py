26df8f1a8619a68fe847be52fe5fc59ad086ea50
seokhyun <gah1234@naver.com>
2022-03-18 16:08:37 +0900

    urls패턴 적용/db연결 후 view를 통해 Post모델 가져오기

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+def index(request):
+
+    posts = Post.objects.all().order_by('-pk')
+    return render(request,'blog/index.html',
+                  {
+                      'posts' : posts,
+                  }
+                  )
+
+# request의 종류에는 여러가지가 있다. 브라우저에서 어떤 요청을 서버에게 줄 수도 있고해서
+# 항상 request를 전달해줘야한다.
