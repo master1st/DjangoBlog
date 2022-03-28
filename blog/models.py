eda2b1050cb79169c1906401a7c95d785903db5a
seokhyun <gah1234@naver.com>
2022-03-28 23:55:10 +0900

    hook 메시지추가 & 템플릿 본문,자기소개 적용

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
+    hook = models.TextField(blank=True)
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
