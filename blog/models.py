8638138611c59b9c1696d8274b7516b9220fc003
seokhyun <gah1234@naver.com>
2022-03-28 22:15:05 +0900

    파일업로드기능 및 파일명 정의

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+import os.path
 from turtle import mode
 from django.db import models
 
@@ -22,3 +23,5 @@ class Post(models.Model):
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
 
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
